dda279e1cd24161dd87a16f7be07ecd959fb5402
Liam Roche <roche.liamp@gmail.com>
2022-05-20 09:13:46 -0400

    model adjustment for thing

--- models/thing.js
+++ models/thing.js
@@ -1,1 +1,14 @@
-
+module.exports = (sequelize, Sequelize) => {
+    const Thing = sequelize.define("thing", {
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      photo: {
+          type: Sequelize.STRING
+      }
+    });
+    return Thing;
+  };
